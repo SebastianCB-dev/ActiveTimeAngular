401b97313c53c532a4ea28d9703e1ad3689507ac
Joan Sebastian Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-03-15 11:39:49 -0500

    Feat: Added GUI and validations with DB in Login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {SoundsService} from "../../services/sounds.service";
+import {FirebaseService} from "../../services/firebase.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -13,9 +16,12 @@ export class LoginComponent implements OnInit {
     "username": [,[Validators.required, Validators.minLength(6), Validators.maxLength(16)]],
     "password": [,[Validators.required, Validators.minLength(6), Validators.maxLength(16)]]
   });
-  isLoginInvalid: boolean = true;
+  isLoginInvalid: boolean = false;
   isLoading = false;
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private soundsService: SoundsService,
+              private fs: FirebaseService,
+              private route: Router) { }
 
   ngOnInit(): void {
     if(this.titlePage) {
@@ -23,12 +29,34 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  login() {
+  async login() {
+    const username = this.myForm.controls['username'].value;
+    const password = this.myForm.controls['password'].value;
+
+    // If the form is invalid
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      this.soundsService.error();
+      return;
+    }
+    this.isLoading = true;
+    // Get User DB
+    const user = await this.fs.getUser(username);
+    if(!user || user['password'] !== password) {
+      this.isLoginInvalid = true;
+      this.isLoading = false;
+      this.soundsService.error();
+      return;
+    }
+    // TODO: Create token UUID
+    this.soundsService.success();
+    this.isLoading = false;
+    await this.route.navigate(['/']);
 
   }
 
   changeValueLogin() {
-
+    this.isLoginInvalid = false;
   }
 
   isInvalidCamp( camp: string ): boolean {
