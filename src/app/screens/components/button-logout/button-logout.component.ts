d3f7e125cb06f29e97acb3c52a90b30bc9df5393
Joan Sebastian Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-03-21 17:24:21 -0500

    Feat: Added button logout

--- src/app/screens/components/button-logout/button-logout.component.ts
+++ src/app/screens/components/button-logout/button-logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-button-logout',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonLogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    localStorage.removeItem('att-session');
+    this.router.navigateByUrl('/');
   }
 
 }
