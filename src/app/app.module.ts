37faf6fb28761f3bc578c769f7a84cb6178db6ad
Joan Sebastian Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-03-23 11:23:56 -0500

    Feat: Added about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import {AppRoutingModule} from "./app-routing.module";
 import { PricesComponent } from './prices/prices.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    PricesComponent
+    PricesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
