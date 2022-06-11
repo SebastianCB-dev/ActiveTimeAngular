e6d4f9122442f6542305f6a45f1a11d2e931f442
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-06-11 10:50:33 -0500

    Feat: Added sounds add task

--- src/app/screens/add-tasks/add-tasks.component.ts
+++ src/app/screens/add-tasks/add-tasks.component.ts
@@ -4,6 +4,7 @@ import { FirebaseService } from '../../services/firebase.service';
 
 // ID
 import { v4 as uuidv4 } from 'uuid';
+import { SoundsService } from 'src/app/services/sounds.service';
 @Component({
   selector: 'app-add-tasks',
   templateUrl: './add-tasks.component.html',
@@ -20,10 +21,11 @@ export class AddTasksComponent implements OnInit {
   })
 
   isValidForm: boolean = true;
-
+  isCreating: boolean = false;
   constructor(
     private fb: FormBuilder,
-    private fs: FirebaseService
+    private fs: FirebaseService,
+    private ss: SoundsService
   ) { }
 
   ngOnInit(): void {
@@ -31,26 +33,31 @@ export class AddTasksComponent implements OnInit {
   }
 
   async addTask() {
-
+    this.isCreating = true;
     if(!this.miFormulario.valid) {
       this.isValidForm = false;
+      this.isCreating = false;
+      this.ss.error();
       return;
     }
-    
+
     const task = {
       id: uuidv4(),
+      user: localStorage.getItem('att-session'),
       name: this.miFormulario.controls["task-name"].value,
       description: this.miFormulario.controls["task-description"].value,
       priority: this.miFormulario.controls["priority"].value,
       initialDate: this.miFormulario.controls["initial-date"].value,
       finalDate: this.miFormulario.controls["final-date"].value
     };
-
     await this.fs.registerTask(task);
+    this.isCreating = false;
+    this.ss.success();
+    this.miFormulario.reset();
   }
 
   clearForm() {
-    
+
     this.miFormulario.reset({
       "task-name": '',
       "task-description": '',
@@ -58,8 +65,8 @@ export class AddTasksComponent implements OnInit {
       "initial-date": '',
       "final-date": ''
     });
-    
-        
+
+
   }
 
   writing() {
