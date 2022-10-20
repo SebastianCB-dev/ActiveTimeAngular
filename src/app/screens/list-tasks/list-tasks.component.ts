1f6fcb4f37aa76d9e7ed0a3326dcbc4bb99b88a3
Sebastian Carrillo <sebastian.carrillob01@gmail.com>
2022-10-20 10:52:19 -0500

    Added UI no tasks in listTasks view

--- src/app/screens/list-tasks/list-tasks.component.ts
+++ src/app/screens/list-tasks/list-tasks.component.ts
@@ -10,6 +10,7 @@ export class ListTasksComponent implements OnInit {
 
   tasks_completed = [];
   tasks_no_completed = [];
+  isLoading: boolean = false;
   value_priority: any = {
     'Low': 0,
     'Medium': 1,
@@ -23,9 +24,11 @@ export class ListTasksComponent implements OnInit {
   }
 
   async getData() {
+    this.isLoading = true;
     const data = await this.fs.getTasksByUser(localStorage.getItem('att-session') || '');
-    this.tasks_completed = this.sortData(data.filter( (task: any) => task['isCompleted'] === true ));
-    this.tasks_no_completed = this.sortData(data.filter( (task: any) => task['isCompleted'] === false ))
+    this.tasks_completed = this.sortData(data.filter((task: any) => task['isCompleted'] === true ));
+    this.tasks_no_completed = this.sortData(data.filter((task: any) => task['isCompleted'] === false ));
+    this.isLoading = false;
   }
 
   sortData(data: any) {
