3f445d83b37af82184ee33bb742060bf8643893e
SebastianCB <sebastian.carrillob01@gmail.com>
2022-10-28 18:08:48 -0500

    Feat: Added Button to updated tasks

--- src/app/screens/update-tasks/update-tasks.component.ts
+++ src/app/screens/update-tasks/update-tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-update-tasks',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateTasksComponent implements OnInit {
 
-  constructor() { }
+  isLoading: boolean = false;
+  tasks: any = [];
+  constructor(private fs: FirebaseService) {
+    this.getTasks();    
+   }
 
   ngOnInit(): void {
   }
 
+  async getTasks() {
+    this.isLoading = true;
+    const data = await this.fs.getTasksByUser(localStorage.getItem('att-session') || '');
+    this.tasks = data;
+    this.isLoading = false;
+    console.log(data);
+  }
+
+  updateTask(index: number) {
+    console.log(index);
+  }
 }
