f0f92821cedffa708652dcf57d16f9d7f88da81e
SebastianCB <seabstian.carrillob01@gmail.com>
2022-11-21 15:08:23 -0500

    Feat: Added sweetaleart2 and added delete task preview

--- src/app/screens/delete-tasks/delete-tasks.component.ts
+++ src/app/screens/delete-tasks/delete-tasks.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from "../../services/firebase.service";
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-delete-tasks',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-tasks.component.css']
 })
 export class DeleteTasksComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  taskCharged: boolean = false;
+  isLoading: boolean = false;
+  tasks = [];
+  constructor(private fs: FirebaseService) {
+    this.getData();
   }
 
+  ngOnInit(): void {}
+
+  async getData() {
+    this.isLoading = true;
+    this.tasks = await this.fs.getTasksByUser(localStorage.getItem('att-session') || '');
+    this.isLoading = false;
+    this.taskCharged = true;
+  }
+
+  deleteTask(task: any) {
+    // Confirmar
+    Swal.fire({
+      title: 'Do you want to delete this task?',
+      text: `Task: ${task['name']}`,
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: 'Yes',
+      denyButtonText: `No`,
+      confirmButtonColor: '#41A10C'
+    }).then(async (result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        console.log('Deleting...');
+        await this.getData();
+      }
+    });
+  }
 }
