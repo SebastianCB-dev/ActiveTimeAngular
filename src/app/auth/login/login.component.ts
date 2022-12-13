07b444d540cb82ac8704280b33f9c97e2962fa77
SebastianCB <sebastian.carrillob01@gmail.com>
2022-12-13 13:18:45 -0500

    Fix: Optimized Login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,34 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 import {UntypedFormBuilder, UntypedFormGroup, Validators} from "@angular/forms";
+
 import {SoundsService} from "../../services/sounds.service";
 import {FirebaseService} from "../../services/firebase.service";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   titlePage: Element = document.querySelector('#title-page')!;
   myForm: UntypedFormGroup = this.fb.group({
-    "username": [,[Validators.required, Validators.minLength(6), Validators.maxLength(16)]],
-    "password": [,[Validators.required, Validators.minLength(6), Validators.maxLength(16)]]
+    "username": [,[Validators.required, 
+                   Validators.minLength(6), 
+                   Validators.maxLength(16)]],
+    "password": [,[Validators.required, 
+                   Validators.minLength(6), 
+                   Validators.maxLength(16)]]
   });
   isLoginInvalid: boolean = false;
-  isLoading = false;
+  isLoading: boolean = false;
+
   constructor(private fb: UntypedFormBuilder,
               private soundsService: SoundsService,
               private fs: FirebaseService,
               private route: Router) { }
 
-  ngOnInit(): void {}
-
   async login() {
-    const username = this.myForm.controls['username'].value;
-    const password = this.myForm.controls['password'].value;
-
+    const username: string = this.myForm.controls['username'].value;
+    const password: string = this.myForm.controls['password'].value;
     // If the form is invalid
     if(this.myForm.invalid) {
       this.myForm.markAllAsTouched();
@@ -38,6 +41,7 @@ export class LoginComponent implements OnInit {
     this.isLoading = true;
     // Get User DB
     const user = await this.fs.getUser(username);
+
     if(!user || user['password'] !== password) {
       this.isLoginInvalid = true;
       this.isLoading = false;
@@ -47,19 +51,16 @@ export class LoginComponent implements OnInit {
     localStorage.setItem('att-session', username);
     this.soundsService.success();
     this.isLoading = false;
-    // Todo change route
     await this.route.navigate(['/tasks/menu']);
-
   }
 
   changeValueLogin() {
     this.isLoginInvalid = false;
   }
 
-  isInvalidCamp( camp: string ): boolean {
+  isInvalidCamp(camp: string): boolean {
     return this.myForm.controls[camp].errors! &&
       this.myForm.controls[camp].touched!;
   }
-
 
 }
